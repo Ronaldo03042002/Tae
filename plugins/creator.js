28ed1334e5d44c7c314c343e8a04f30618bd7eaf
Ronaldo03042002 <77816080+Ronaldo03042002@users.noreply.github.com>
2021-02-16 19:11:36 -0300

    Delete plugins directory

--- plugins/creator.js
+++
@@ -1,9 +0,0 @@
-let handler = function (m) {
-  this.sendContact(m.chat, '557196834782', 'Ronaldo', m)
-}
-handler.help = ['dono', 'criador']
-handler.tags = ['info']
-
-handler.command = /^(dono|criador)$/i
-
-module.exports = handler
